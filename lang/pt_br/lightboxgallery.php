7478ee9f79a1f1f2e858bf3fcaafbe8bd5c4fac6
Andre Yamin <andreyamin@gmail.com>
2012-07-07 18:25:10 -0300

    brazilian translation

---
+++ lang/pt_br/lightboxgallery.php
@@ -0,0 +1,125 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://moodle.labtk.com.br
+ *
+ * @package    mod
+ * @subpackage lightboxgallery
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['acceptablefiletypebriefing'] = 'Se você quer enviar vários arquivos de uma só vez, você pode compactar localmente uma pasta contendo imagens e enviar. Assim todas as imagens do .zip serão adicionadas à galeria.';
+$string['addcomment'] = 'Adicionar comentário';
+$string['addimage'] = 'Adicionar imagens';
+$string['addimage_help'] = 'Busque uma imagem na sua máquina local para adicioná-la à esta galeria.';
+$string['allowcomments'] = 'Permitir comentários';
+$string['allowrss'] = 'Permitir feeds RSS';
+$string['allpluginsdisabled'] = 'Lamento, todos os plugins de edição estão desabilitados';
+$string['autoresize'] = 'Redimensionamento automático';
+$string['autoresize_help'] = 'Você pode controlar se e como as imagens da galeria são redimensionados. Os seguintes métodos estão disponíveis ao configurar uma galeria: * Tela: imagens que são maiores que a tela de usuários será reduzida para caber dentro da tela. * Upload: as imagens serão redimensionadas para dimensões especificadas quando são carregados através da opção "Adicionar imagem". Há também um plugin de redimensionamento de imagem incluídas no editor de imagem, onde você pode redimensionar manualmente imagens.';
+$string['backtogallery'] = 'Voltar à galeria';
+$string['captionfull'] = 'Exibir texto da legenda completo';
+$string['captionpos'] = 'Posição da legenda';
+$string['commentadded'] = 'Seu comentário foi enviado à galeria';
+$string['commentcount'] = '{$a} comentários';
+$string['commentdelete'] = 'Confirma que quer apagar o comentário?';
+$string['configdisabledplugins'] = 'Plugins desabilitados';
+$string['configdisabledpluginsdesc'] = 'Selecione os plugins de edição de imagens que você quer desabilitar.';
+$string['configenablerssfeeds'] = 'Habilitar feeds RSS';
+$string['configenablerssfeedsdesc'] = 'Permitir que feeds RSS sejam gerados a partir de galerias.';
+$string['configimagelifetime'] = 'Tempo de vida da imagem';
+$string['configimagelifetimedesc'] = 'Intervalo de tempo (em segundos) que imagens ficam no cache do navegador.';
+$string['configoverwritefiles'] = 'Sobrescrever arquivos';
+$string['configoverwritefilesdesc'] = 'Sobrescreve imagens quando novas imagens são enviadas com o mesmo nome de arquivo.';
+$string['configstrictfilenames'] = 'Usar nomes de arquivo restritos';
+$string['configstrictfilenamesdesc'] = 'Forçar editor de imagem a limpar os nomes de arquivo de acordo com as regras de nomeação do Moodle.';
+$string['currentsize'] = 'Tamanho atual';
+$string['dimensions'] = 'Dimensões';
+$string['dirdown'] = 'Para baixo';
+$string['dirleft'] = 'Esquerda';
+$string['dirright'] = 'Direita';
+$string['dirup'] = 'Para cima';
+$string['displayinggallery'] = 'Galeria:';
+$string['edit_caption'] = 'Legenda';
+$string['edit_crop'] = 'Cortar';
+$string['edit_delete'] = 'Apagar';
+$string['edit_flip'] = 'Inverter';
+$string['edit_resize'] = 'Redimensionar';
+$string['edit_resizescale'] = 'Escalar';
+$string['edit_rotate'] = 'Rotacionar';
+$string['edit_tag'] = 'Tag';
+$string['edit_thumbnail'] = 'Miniatura';
+$string['editimage'] = 'Editar imagem';
+$string['errornofile'] = 'O arquivo requisitado não foi encontrado: {$a}';
+$string['errornoimages'] = 'Nenhuma imagem foi encontrada nesta galeria';
+$string['errornosearchresults'] = 'Sua busca não encontrou nenhuma imagem';
+$string['erroruploadimage'] = 'Os arquivos enviados precisam ser arquivos de imagem';
+$string['extendedinfo'] = 'Mostrar informação da imagem extendida';
+$string['imageadd'] = 'Adicionar imagens';
+$string['imagecount'] = 'Contagem de imagens';
+$string['imagecounta'] = '{$a} imagens';
+$string['imagedirectory'] = 'Diretório de imagens';
+$string['imagedirectory_help'] = 'Selecione o diretório que contem as imagens que você quer exibir na galeria. Ao usar a opção \'Adicionar imagem\', as imagens adicionadas serão colocadas nesete diretório.';
+$string['imagedownload'] = 'Baixar imagem';
+$string['imageresized'] = 'Imagem redimensionada:{$a}';
+$string['images'] = 'Imagens';
+$string['imagesperpage'] = 'Imagens por página';
+$string['imagesperrow'] = 'Imagens por linha';
+$string['imageuploaded'] = 'Imagem enviada: {$a}';
+$string['invalidquizid'] = 'Id de Galeria inválido';
+$string['lightboxgallery'] = 'Galeria de imagens';
+$string['lightboxgallery:addcomment'] = 'Adicionar comentário à galeria de imagens';
+$string['lightboxgallery:addimage'] = 'Adicionar imagem à galeria de imagens';
+$string['lightboxgallery:addinstance'] = 'Adicionar uma nova galeria de imagens';
+$string['lightboxgallery:edit'] = 'Editar galeria de imagens';
+$string['lightboxgallery:submit'] = 'Enviar uma galeria de imagens';
+$string['lightboxgallery:viewcomments'] = 'Ver comentários às galerias de imagem';
+$string['makepublic'] = 'Tornar público';
+$string['metadata'] = 'Metadados';
+$string['modulename'] = 'Galeria de imagens';
+$string['modulenameadd'] = 'Galeria de imagens';
+$string['modulenameplural'] = 'Galeria de imagens';
+$string['modulenameshort'] = 'Galeria';
+$string['newgallerycomments'] = 'Novos comentários na galeria';
+$string['norssfeedavailable'] = 'Feed não disponível';
+$string['pluginadministration'] = 'Administração da galeria de imagens';
+$string['pluginname'] = 'Galeria de imagens';
+$string['position_bottom'] = 'Em baixo';
+$string['position_top'] = 'Em cima';
+$string['resizeto'] = 'Redimensionar para';
+$string['rsssubscribe'] = 'Feed RSS da galeria';
+$string['saveimage'] = 'Salvar {$a}';
+$string['screen'] = 'Tela';
+$string['selectflipmode'] = 'Selecione modo de inversão';
+$string['selectrotation'] = 'Selecione ângulo de rotação';
+$string['selectthumbpos'] = 'Deslocamento da miniatura (a partir do centro)';
+$string['setasindex'] = 'Definir como imagem de índice';
+$string['showall'] = 'Mostrar todas';
+$string['tagscurrent'] = 'Tags atuais';
+$string['tagsdisabled'] = 'Editor de tags está desabilitado';
+$string['tagsimport'] = 'Importar tags';
+$string['tagsimportconfirm'] = 'Você tem certeza que quer importar tags de todas as imagens nesta galeria?';
+$string['tagsimportfinish'] = 'Terminou a importação {$a->tags} tags de {$a->images} imagens';
+$string['tagsiptc'] = 'Tags IPTC';
+$string['tagspopular'] = 'Tags populares';
+$string['tagsrelated'] = 'Tags relacionados';
+$string['thumbnailoffset'] = 'Deslocamento';
+$string['zipextracted'] = 'Arquivo zip extraído: {$a}';
+$string['zipnonewfiles'] = 'Nenhuma imagem nova foi encontrada - certifique-se de que as imagens estão do diretório raiz do arquivo';
